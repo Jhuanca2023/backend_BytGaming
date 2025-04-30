13a6b93b9773c95ce68fccf3d824b6f57aa7531d
RogerCll06 <concepcionleonrogererik@gmail.com>
2025-04-30 01:12:28 -0500

    refactor: Remove unused authentication DTOs and exception handling classes

--- src/main/java/gaming/pe/DTOs/auth/AuthResponseRegisterDto.java
+++
@@ -1,9 +0,0 @@
-package gaming.pe.DTOs.auth;
-
-import jakarta.validation.constraints.Size;
-
-import java.util.List;
-
-public record AuthResponseRegisterDto(
-        @Size(max = 3, message = "The user cannot have more than 3 roles") String roleListName) {
-}
--- src/main/java/gaming/pe/exceptions/InvalidFileException.java
+++
@@ -1,7 +0,0 @@
-package gaming.pe.exceptions;
-
-public class InvalidFileException extends RuntimeException {
-    public InvalidFileException(String message) {
-        super(message);
-    }
-}
